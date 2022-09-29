9b02536745c7ebb084d5e23e8160937400f1de6e
Andrey Vasilenko <vasilenko.andrey77@outlook.com>
2022-09-30 02:28:19 +0700

    Added DB initialize + super admin

---
+++ MiniDevTo/Migrations/_001_seed_initial_admin_account.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Entities;
+
+namespace MiniDevTo.Migrations;
+
+public class _001_seed_initial_admin_account : IMigration
+{
+    internal static string SuperAdminPassword { get; set; }
+
+    public async Task UpgradeAsync()
+    {
+        await new Dom.Admin
+        {
+            Email = "admin@mail.com",
+            ID = "SUPERADMIN01",
+            PasswordHash = BCrypt.Net.BCrypt.HashPassword(SuperAdminPassword),
+            UserName = "OstapBander"
+        }.SaveAsync();
+    }
+}
--- MiniDevTo/Program.cs
+++ MiniDevTo/Program.cs
@@ -1,8 +1,10 @@
 global using FastEndpoints;
 global using FluentValidation;
+global using MongoDB.Entities;
 using FastEndpoints.Swagger;
+using MiniDevTo.Migrations;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder();
 builder.Services.AddFastEndpoints();
 builder.Services.AddSwaggerDoc();
 
@@ -13,5 +15,9 @@ app.UseAuthorization();
 app.UseFastEndpoints();
 app.UseOpenApi();
 app.UseSwaggerUi3(c => c.ConfigureDefaults());
+
+await DB.InitAsync(database: "MiniDevTo", host: "localhost");
+_001_seed_initial_admin_account.SuperAdminPassword = app.Configuration["SuperAdminPassword"];
+await DB.MigrateAsync();
 
 app.Run();
